79632b61faa29bf027877a477668bb43fbb94e02
lunchboxer <zixingchepeng@163.com>
2017-11-28 12:56:03 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('hi')
+// most popular baby names of 2017 in order of popularity according to
+// https://www.babycenter.com/top-baby-names-2017.htm
+const GIRLNAMES = ['Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia', 'Mia', 'Amelia', 'Charlotte', 'Harper', 'Ella', 'Arria', 'Evelyn', 'Abigail', 'Emily', 'Avery', 'Scarlett', 'Madison', 'Sofia', 'Chloe', 'Lily']
+const BOYNAMES = ['Liam', 'Noah', 'Logan', 'Lucas', 'Mason', 'Oliver', 'Ethan', 'Elijah', 'Aiden', 'James', 'Benjamin', 'Sebastian', 'Jackson', 'Alexander', 'Jacob', 'Cater', 'Jayden', 'Michael', 'Daniel', 'Luke']
+
+let girlButton = document.getElementById('girlButton')
+let boyButton = document.getElementById('boyButton')
+let output = document.getElementById('name')
+
+function outputName(namesArray) {
+  let index = Math.floor(Math.random() * namesArray.length)
+  output.innerHTML = namesArray[index]
+}
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function() {
+
+  console.log("document Ready");
+  girlButton.addEventListener('click', function() {
+    outputName(GIRLNAMES)
+  }, false);
+  boyButton.addEventListener('click', function() {
+    outputName(BOYNAMES)
+  }, false);
+});
